4ad8d93fea82bb090fd6f514155d6e8b4347b607
Shun Yu <shunsmailbox@gmail.com>
2019-11-24 14:09:35 -0800

    BR-3 Combine Chinese translation

--- src/compare.py
+++ src/compare.py
@@ -5,9 +5,10 @@ from difflib import SequenceMatcher
 
 import overlap
 
-def compare(infile_a, infile_b, outfile):
+def compare(infile_c, infile_a, infile_b, outfile):
   # print("%s, %s, %s" % (infile_a, infile_b, outfile))
 
+  sub_c = webvtt.from_sbv(infile_c)
   sub_a = webvtt.from_sbv(infile_a)
   sub_b = webvtt.from_sbv(infile_b)
 
@@ -36,9 +37,22 @@ def compare(infile_a, infile_b, outfile):
         if c[1] == 1:
           data.append([_b.start, _b.end, "-", "-", _b.text, ""])
         else:
-          data.append(["-", "-", _b.start, _b.end, "", _b.text])
+          data.append([_b.start, _b.end, _b.start, _b.end, "", _b.text])
 
-  df = pd.DataFrame(data, columns = ["start", "end", "start", "end", "old", "new", "word change"])
+  data_c = [[sub.start, sub.end, sub.text] for sub in sub_c]
+  df_c = pd.DataFrame(data_c, columns = ["start", "end", "Chinese"])
+  df_ = pd.DataFrame(data, columns = [
+    "start", "end", "start (revised)", "end (revised)", "Translation", "Revised", "word change"])
+
+  df_c = df_c.set_index(["start", "end"])
+  df_ = df_.set_index(["start", "end"])
+
+  df = df_c.join(df_, how='outer')
+  df = df.reset_index(level=["start", "end"])
+  df = df[["start", "end", "start (revised)", "end (revised)", "Chinese", "Translation", "Revised", "word change"]]
+
+  df.loc[df["Translation"] == "", ["start", "end"]] = "-"
+
   _to_excel(outfile, df)
 
 def _to_excel(outfile, df):
@@ -52,8 +66,8 @@ def _to_excel(outfile, df):
   f_wrap = workbook.add_format({'text_wrap': True})
   f_wc = workbook.add_format({'num_format': '0.00'})
   worksheet.set_column('A:D', 24)
-  worksheet.set_column('E:F', 55, f_wrap)
-  worksheet.set_column('G:G',12, f_wc)
+  worksheet.set_column('E:G', 55, f_wrap)
+  worksheet.set_column('H:H',12, f_wc)
 
   f_green = workbook.add_format({
     'bg_color': '#C6EFCE',
@@ -68,14 +82,14 @@ def _to_excel(outfile, df):
     'font_color': '#9C6500'
   })
 
-  worksheet.conditional_format('G2:G1048576', {
+  worksheet.conditional_format('H2:H1048576', {
     'type': 'cell',
     'criteria': '<',
     'value': 0.4,
     'format': f_green
   })
 
-  worksheet.conditional_format('G2:G1048576', {
+  worksheet.conditional_format('H2:H1048576', {
     'type': 'cell',
     'criteria': 'between',
     'minimum': 0.4,
@@ -83,7 +97,7 @@ def _to_excel(outfile, df):
     'format': f_red
   })
 
-  worksheet.conditional_format('G2:G1048576', {
+  worksheet.conditional_format('H2:H1048576', {
     'type': 'cell',
     'criteria': '>',
     'value': 0.99,
@@ -94,7 +108,7 @@ def _to_excel(outfile, df):
 if __name__ == '__main__':
   import sys
 
-  if len(sys.argv) < 3:
+  if len(sys.argv) < 4:
     raise Exception("Missing arguments.")
 
-  compare(*sys.argv[1:4])
+  compare(*sys.argv[1:5])
