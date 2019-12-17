9ac2e5313d1d25f9ee2d617780225937eb3ebdfc
Shun Yu <shunsmailbox@gmail.com>
2019-12-16 20:58:41 -0800

    Adding paths.py

---
+++ src/paths.py
@@ -0,0 +1,6 @@
+CHINESE_FILE = ""
+ORIGINAL_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\Youtube LeTV Published\\EP20volunteers.sbv"
+REVISED_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\Youtube LeTV Published\\EP20 Sept21.sbv"
+OUT_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\测试 培训\\Carsen修改前后对比\\EP20组会20190922.xlsx"
+
+PATHS = [CHINESE_FILE, ORIGINAL_FILE, REVISED_FILE, OUT_FILE]
