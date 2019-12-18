caa19282a421e6b7d8bd4dde80c26e1a7ba6fde8
Jiachen Wei <42905348+CarsenWei@users.noreply.github.com>
2019-12-18 13:23:39 -0800

    Create videocr Try It.py

---
+++ videocr Try It.py
@@ -0,0 +1,10 @@
+#Install Tesseract and make sure it is in your $PATH
+    #https://github.com/tesseract-ocr/tesseract/wiki
+    #Open Environment Variable, add the folder path to path
+#!pip install videocr
+    #https://pypi.org/project/videocr/
+    #https://github.com/apm1467/videocr
+from videocr import get_subtitles
+if __name__ == '__main__':  # This check is mandatory for Windows.
+    print(get_subtitles("C:\\Users\\jwei06\\Downloads\\ChinaNowEP01.mp4",
+                        lang='chi_sim', sim_threshold=70, conf_threshold=65))
