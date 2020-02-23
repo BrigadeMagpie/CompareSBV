f53313d6207310c72d84476fed4c232b72b4f945
Jiachen Wei <42905348+CarsenWei@users.noreply.github.com>
2020-02-22 17:42:05 -0800

    Update paths.py

--- src/paths.py
+++ src/paths.py
@@ -1,9 +1,9 @@
-CHINESE_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\Youtube LeTV Published\\4509 Templates with Chinese subs\\Empress In The Palace (YouTube Template) - E27 - converted.sbv"
+CHINESE_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\CompareSBV\\docs\\EP21Chinese.sbv"
 
-ORIGINAL_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\Youtube LeTV Published\\EP27V0220.sbv"
+ORIGINAL_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\CompareSBV\\docs\\EP21Volunteers.sbv"
 
-REVISED_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\Youtube LeTV Published\\EP27Carsen0222.sbv"
+REVISED_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\CompareSBV\\docs\\EP21Carsen.sbv"
 
-OUT_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\测试 培训\\Carsen修改前后对比\\EP27组会.xlsx"
+OUT_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\CompareSBV\\resource\\EP21Output.xlsx"
 
 PATHS = [CHINESE_FILE, ORIGINAL_FILE, REVISED_FILE, OUT_FILE]
