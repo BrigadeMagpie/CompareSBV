6ab4cf6d1a97b956d3bb94b84b82e876f26eb192
Jiachen Wei <42905348+CarsenWei@users.noreply.github.com>
2020-08-17 21:04:16 -0700

    Update paths.py
    Added encoding info

--- src/paths.py
+++ src/paths.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 CHINESE_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\CompareSBV\\docs\\EP21Chinese.sbv"
+#CHINESE_FILE=""  #To compare only 2 sbv siles
 
 ORIGINAL_FILE = "C:\\Users\\Jiachen\\OneDrive\\YouTube Subtitles\\CompareSBV\\docs\\EP21Volunteers.sbv"
 
